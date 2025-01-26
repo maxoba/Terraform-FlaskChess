3a1be32cd0c1216a136224f6def766be32d65ee8
max <maxoba@yahoo.com>
2025-01-26 00:59:30 -0700

    7th

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "time_sleep" "wait_for_instance" {
 }
 
 resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
+  name        = "allow_ssh_http_new"
   description = "Allow SSH and HTTP traffic"
   vpc_id      = "vpc-0293333d27a5d4165"
 
